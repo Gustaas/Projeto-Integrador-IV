3af7ee42e28bc786e3bb16d41d09e093266d06ab
Gustaas <contatogustavo0743@gmail.com>
2024-08-31 18:30:44 -0300

    Teste

---
+++ Projeto/src/main/java/com/example/pi/ProjetoPiLojaDeVinhoApplication.java
@@ -0,0 +1,12 @@
+package com.example.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPiLojaDeVinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPiLojaDeVinhoApplication.class, args);
+	}
+}
